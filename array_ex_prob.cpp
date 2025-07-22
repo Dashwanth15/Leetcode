cc930fbb2ec43aa73448e18977cac0fdc17b5bc0
Dashwanth Madduri <dashwanthmadduri@gmail.com>
2025-07-22 21:13:02 +0530

    Update array_ex_prob.cpp

--- array_ex_prob.cpp
+++ array_ex_prob.cpp
@@ -1,3 +1,7 @@
+// 1.two sum problem
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
 #include<iostream>
 using namespace std;
 int main(){
